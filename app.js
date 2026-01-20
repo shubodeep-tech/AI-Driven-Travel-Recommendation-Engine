c2b25b11155d9e6c971b4c296fdfdeac0e0015c4
shubodeep-tech <shubodeep.k.das@gmail.com>
2026-01-20 12:10:24 +0530

    Day 1:Project setup

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ async function main() {
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
--- models/listing.js
+++ models/listing.js
@@ -16,11 +16,9 @@ const listingSchema = new Schema({
     },
     url: {
       type: String,
-      default:
-        "https://images.unsplash.com/photo-1508057198894-247b23fe5ade?auto=format&fit=crop&w=800&q=60",
+      default:"https://images.unsplash.com/photo-1508057198894-247b23fe5ade?auto=format&fit=crop&w=800&q=60",
       set: (v) =>
-        v === "" || v === null || v === undefined
-          ? "https://images.unsplash.com/photo-1508057198894-247b23fe5ade?auto=format&fit=crop&w=800&q=60"
+        v === "" || v === null || v === undefined? "https://images.unsplash.com/photo-1508057198894-247b23fe5ade?auto=format&fit=crop&w=800&q=60"
           : v,
     },
   },
