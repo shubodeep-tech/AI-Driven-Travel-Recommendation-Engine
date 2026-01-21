01e735d7032eebe7a667ac7ce532bdf8d11417ff
shubodeep-tech <shubodeep.k.das@gmail.com>
2026-01-22 00:04:40 +0530

    Day 5:– Reviews & Mongoose Middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/expressError");
 const {listingSchema} = require("./schema.js");
+const Review= require("./models/reviews.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -105,6 +106,20 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   await Listing.findByIdAndDelete(req.params.id);
   res.redirect("/listings");
 }));
+
+//Reviews 
+//Post Route
+app.post("/listings/:id/reviews",async(req,res)=>{
+  let  listing = await Listing.findById(req.params.id);
+  let newReview =new Review(req.body.review);
+  listing.reviews.push(newReview);
+
+   await newReview.save();
+   await listing.save();
+
+  res.redirect(`/listings/${listing._id}`);
+
+});    
 
 
 app.use((req, res, next) => {
--- models/listing.js
+++ models/listing.js
@@ -26,6 +26,13 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews:[
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref :"Review",
+
+    }
+  ]
 });
 
 module.exports = mongoose.model("Listing", listingSchema);
