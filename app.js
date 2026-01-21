f195eafbb227b87b13a94e49d7226357faa76a2a
shubodeep-tech <shubodeep.k.das@gmail.com>
2026-01-21 09:40:36 +0530

    :Day 4 – Error Handling & Validation

--- app.js
+++ app.js
@@ -5,33 +5,51 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/expressError");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
+// DB connection
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+main()
+  .then(() => console.log("connected to db"))
+  .catch(err => console.log(err));
 
+// View engine
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
-main()
-  .then(() => console.log("connected to db"))
-  .catch((err) => console.log(err));
-
+// Root
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-// INDEX – All listings
-app.get("/listings", async (req, res) => {
+const validateListing =(req,res,next) =>{
+  let {error} = listingSchema.validate(req.body);
+  if(error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400,errMsg)
+  }else{
+    next();
+  }
+
+ }
+
+// INDEX
+app.get("/listings", wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index", { allListings });
-});
+}));
 
 // NEW
 app.get("/listings/new", (req, res) => {
@@ -39,34 +57,65 @@ app.get("/listings/new", (req, res) => {
 });
 
 // SHOW
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
   const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    throw new ExpressError(404, "Listing not found");
+  }
   res.render("listings/show", { listing });
-});
+}));
 
 // CREATE
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings", validateListing,
+   wrapAsync(async (req, res) => {
+    listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+      throw new ExpressError(400,result.error);
+    }
+
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+
+);
 
 // EDIT
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
   const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    throw new ExpressError(404, "Listing not found");
+  }
   res.render("listings/edit", { listing });
-});
+}));
 
 // UPDATE
-app.put("/listings/:id", async (req, res) => {
-  await Listing.findByIdAndUpdate(req.params.id, req.body.listing);
-  res.redirect(`/listings/${req.params.id}`);
-});
+app.put("/listings/:id", 
+  validateListing,
+  wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+}));
 
 // DELETE
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
   await Listing.findByIdAndDelete(req.params.id);
   res.redirect("/listings");
+}));
+
+
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
+
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs",{message})
+  //res.status(statusCode).send(message);
 });
 
 app.listen(8060, () => {
