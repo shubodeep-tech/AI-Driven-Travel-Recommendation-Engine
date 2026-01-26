391bdb3cee1749964e56bcc59ad1569340e5a0fa
shubodeep-tech <shubodeep.k.das@gmail.com>
2026-01-26 21:29:46 +0530

    Sessions, Cookies & Flash Messages

--- schema.js
+++ schema.js
@@ -1,15 +1,17 @@
 const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-        location:Joi.string().required(),
-        country:Joi.string().required(),
-        price:Joi.number().required().min(0),
-        image:Joi.string().allow("",null)
-    }).required()
-})
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.object({
+      url: Joi.string().uri().allow("", null)
+    }).required(),
+    price: Joi.number().required().min(0),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+  }).required()
+});
 
 module.exports.reviewSchema=Joi.object({
     review: Joi.object({
